f76af24df10b7dcdc1253dc184f139ec3d8b0694
mouadh7belgoumri <m_belgoumri@estin.dz>
2025-08-03 12:06:30 +0100

    deleting .js files

--- estin_commerce_backend/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var products_js_1 = require("./routes/products.js");
-// configDotenv()
-var app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use('/api/products', products_js_1.productsRouter);
-app.listen(8000, function () { return console.log("server running on http://localhost:8000/"); });
