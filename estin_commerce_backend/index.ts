19deb02c8f94ae84a0131ae586ddef049de8b13a
mouadh7belgoumri <m_belgoumri@estin.dz>
2025-08-03 12:02:41 +0100

    add and get all products routes

--- estin_commerce_backend/index.ts
+++ estin_commerce_backend/index.ts
@@ -3,12 +3,11 @@ import type { Request, Response } from "express";
 import { configDotenv } from "dotenv";
 import type { Http2ServerRequest, Http2ServerResponse } from "http2";
 import { PrismaClient } from "./generated/prisma/index.js";
+import {productsRouter} from './routes/products.js'
 
-configDotenv()
+// configDotenv()
 const app = express();
-
-
-
-app.get('/',async (req:Request, res:Response)=> {})
+app.use(express.json());
+app.use('/api/products', productsRouter )
 
 app.listen(8000,  ()=>console.log("server running on http://localhost:8000/"))
