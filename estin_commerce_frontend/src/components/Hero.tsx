680f9e3d8e21c5950df63f6bd02e3ad181dd2e96
mouadh7belgoumri <m_belgoumri@estin.dz>
2025-08-11 16:01:44 +0100

    adding the links for my accounts on social media, adjusting the text size for navBar and fixing the hero button link issue(positionning)

--- estin_commerce_frontend/src/components/Hero.tsx
+++ estin_commerce_frontend/src/components/Hero.tsx
@@ -1,13 +1,16 @@
 
 // import { Link } from 'react-router'
+import { Link } from 'react-router'
 import heroImg from '../assets/Group 14.png'
 const Hero = () => {
   return (
     <div className='flex justify-around my-20 '>
-      <div className='content-center ml-15 w-1/3'>
+      <div className='flex flex-col content-center justify-center -mt-20 ml-15 w-1/3'>
         <h1 className='font-bold text-6xl mb-7 '>Maykhosek Walo</h1>
-        <p className='text-2xl'>My goal is to provide you with everything you would natrually struggle to get.</p>
-        <button  className='bg-black text-white rounded-full py-4 px-7 mt-7 text-4xl font-semibold hover:cursor-pointer'>Go Shopping</button>
+        <div className='flex flex-col'>
+          <p className='text-2xl'>My goal is to provide you with everything you would natrually struggle to get.</p>
+          <Link to={'/shop'} className='bg-black text-white rounded-full py-4 px-7 mt-7 text-4xl font-semibold hover:cursor-pointer w-fit'>Go Shopping</Link>
+        </div>
       </div>
       <img src={heroImg} alt="hero image" className='w-3xl ' />
     </div>
