fb09516f91f63da4d65ceaea0c08c71284de63f2
mouadh7belgoumri <m_belgoumri@estin.dz>
2025-08-11 12:14:28 +0100

    cleaning up react test files, creating header component and iniatlizing the hero section component

---
+++ estin_commerce_frontend/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { FaCircleUser } from "react-icons/fa6"
+import { FaCartShopping } from "react-icons/fa6"
+import { Link } from "react-router"
+const Header = () => {
+    return (
+        <div>
+            <div className="flex flex-row justify-between my-4 mx-10 ">
+                <h1><Link to={'/'}>logo </Link></h1>
+                <div className="flex gap-30">
+                    <ul className="flex flex-row gap-15">
+                        <li><Link to={'/'}>Home</Link></li>
+                        <li><Link to={'/shop'}>Shop</Link></li>
+                        <li><Link to={'/suggest'}>Suggest</Link></li>
+                        <li><Link to={'/about'}>About us</Link></li>
+                    </ul>
+                    <div className="flex gap-10">
+                        <FaCartShopping className="text-3xl text-[]"/>
+                        <FaCircleUser className="text-3xl"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- estin_commerce_frontend/src/main.tsx
+++ estin_commerce_frontend/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element ={<App/>} />
+        {/* <Route path='/shop' element ={<Shop/>} /> */}
+        {/* <Route path='/about' element ={<About/>} /> */}
+        {/* <Route path='/suggest' element ={<SuggestProduct/>} /> */}
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
